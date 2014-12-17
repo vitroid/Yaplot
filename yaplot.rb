4d6f36efef2dfd652cf1ed5ead2ee8bfcb31f424
vitroid <vitroid@gmail.com>
2014-12-17 19:48:39 +0900

    .

--- yaplot.rb
+++ yaplot.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Yaplot < Formula
   homepage "https://github.com/vitroid/Yaplot"
-  url "https://github.com/vitroid/Yaplot/archive/v3.5.002.tar.gz"
+  url "https://github.com/vitroid/Yaplot/archive/v3.6.002.tar.gz"
   sha1 "a759c411891701b3b18d89cc297077b73c12bfa4"
 
   depends_on "gtk+"
