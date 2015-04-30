0f80dd9f51e356ea0687f7c8bae379035d48d4e3
vitroid <vitroid@gmail.com>
2015-04-30 09:43:11 +0900

    Timer interval is changed from msec to usec.
    Recent computers are so fast that idle loop finishes less than 1 ms;
    accumulation of 0 ms does not change the timer counter and that affect
    the animations.  Timer accuracy is changed from msec to usec to solve
    the problem.

--- unixwrap.h
+++ unixwrap.h
@@ -2,7 +2,8 @@
 #define _UNIXWRAP_H
 #include "common.h"
 void resettimer();
-int deltatime();
+int deltatime_obsolete();
+int deltatime2();
 Winfo *GetOptions(int argc,char *argv[],Ainfo *a,Ginfo *g);
 int ePushView(Ginfo *g,Winfo w[],int i);
 int ePopView(Ginfo *g,Winfo w[],int i);
