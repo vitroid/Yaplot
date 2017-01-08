768faa974f0d02542bd1daeac34b959f33a2534f
vitroid <vitroid@gmail.com>
2017-01-08 13:14:39 +0900

    Changed antialias policy from NONE to FAST.

--- gtk.c
+++ gtk.c
@@ -166,35 +166,14 @@ void clearfb(Winfo *w)
 
 void exposefb(Winfo *w)
 {
-  /*
-  GdkRectangle rect;
-  gdk_draw_pixmap(w->drawarea->window,
-		  w->drawarea->style->fg_gc[GTK_WIDGET_STATE(w->drawarea)],
-		  w->pixmap,
-		  0,0,
-		  0,0,
-		  w->drawarea->allocation.width,
-		  w->drawarea->allocation.height);
-  */
   cairo_t* cr = gdk_cairo_create(gtk_widget_get_window(w->drawarea));
   if(debug){fprintf(stderr,"exposefb: %d %d\n", (int)cr,(int)w->drawarea);}
-  //cairo_move_to (cr, 30, 30);
-  //cairo_show_text (cr, "Text");
 
-  //cairo_destroy (cr);
   cairo_set_source_surface(cr, w->surface, 0, 0);
   if(debug)
     fprintf(stderr,"REQ::Expose::%d %d\n",
 	  w->drawarea->allocation.width,
 	  w->drawarea->allocation.height);
-  /*make expose event*/
-  /*
-  rect.x=0;
-  rect.y=0;
-  rect.width=w->drawarea->allocation.width;
-  rect.height=w->drawarea->allocation.height;
-  gtk_widget_draw(w->window,&rect);
-  */
   cairo_paint(cr);
   cairo_destroy (cr);
   
@@ -221,19 +200,8 @@ static gint expose_event(GtkWidget *widget,
   if(debug)
     fprintf(stderr,"Expose[%d]::%d %d\n",
 	  i,		  event->area.width,event->area.height);
-  /*
-  gdk_draw_pixmap(widget->window,
-		  widget->style->fg_gc[GTK_WIDGET_STATE(widget)],
-		  w_internal[i].pixmap,
-		  event->area.x,event->area.y,
-		  event->area.x,event->area.y,
-		  event->area.width,event->area.height);*/
   cairo_t* cr = gdk_cairo_create(gtk_widget_get_window (widget));
   if(debug) fprintf(stderr,"expose_event %d\n",(int)cr);
-  //cairo_move_to (cr, 30, 30);
-  //cairo_show_text (cr, "Text");
-
-  //cairo_destroy (cr);
   cairo_set_source_surface(cr, w_internal[i].surface, 0, 0);
   //cairo_paint(cr);
   cairo_destroy (cr);
@@ -246,20 +214,9 @@ static gint configure_event(GtkWidget *widget,
 			 GdkEventConfigure *event)
 {
   int i=WhichWidget(widget);
-  /*
-  if(w_internal[i].pixmap){
-    gdk_pixmap_unref(w_internal[i].pixmap);
-  }
-  */
   if (w_internal[i].surface){
     cairo_surface_finish(w_internal[i].surface);
   }
-  /*
-  w_internal[i].pixmap=gdk_pixmap_new(widget->window,
-				      widget->allocation.width,
-				      widget->allocation.height,
-				      -1);
-  */
   w_internal[i].surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, widget->allocation.width, widget->allocation.height);
   cairo_t* cr  = cairo_create(w_internal[i].surface);
   w_internal[i].cr = cr;
@@ -269,7 +226,7 @@ static gint configure_event(GtkWidget *widget,
   cairo_select_font_face (cr, "Helvetica",
                           CAIRO_FONT_SLANT_ITALIC ,
                           CAIRO_FONT_WEIGHT_NORMAL);
-  cairo_set_antialias(cr, CAIRO_ANTIALIAS_NONE);//for speed
+  cairo_set_antialias(cr, CAIRO_ANTIALIAS_FAST);//for speed
   w_internal[i].screenwidth = widget->allocation.width;
   w_internal[i].screenheight = widget->allocation.height;
   w_internal[i].status|=REDRAW;
@@ -286,6 +243,7 @@ static gint motion_notify_event(GtkWidget *widget,
 {
   int x,y;
   GdkModifierType state;
+  if(debug) fprintf(stderr,"Motion::\n");
 
   if(event->is_hint){
     gdk_window_get_pointer(event->window,&x,&y,&state);
@@ -298,7 +256,6 @@ static gint motion_notify_event(GtkWidget *widget,
     if( (originx !=x) || (originy != y)){
       int i;
       float head,bank;
-      /*fprintf(stderr,"motion::%d %d\n",originx-x,originy-y);*/
       bank = +(originy - y)*(10.0/400.0);
       head = +(originx - x)*(10.0/400.0);
       originx = x;
@@ -320,6 +277,7 @@ static gint button_press_event(GtkWidget *widget,
 				GdkEventButton *event)
 {
   int i;
+  if(debug) fprintf(stderr,"Pressed::\n");
   
   originx=event->x;
   originy=event->y;
@@ -338,8 +296,7 @@ static gint button_release_event(GtkWidget *widget,
 				GdkEventButton *event)
 {
   int i;
-  if(debug)
-    fprintf(stderr,"Released::\n");
+  if(debug) fprintf(stderr,"Released::\n");
   i=WhichWidget(widget);
   if(w_internal[i].async){
     w_internal[i].wp=w_internal[i].wb=w_internal[i].wh=0;
@@ -614,53 +571,50 @@ void W_Init2(Winfo *w,Ginfo *g)
     int i,j;
     g_internal=g;
     w_internal=w;
-    //if(debug)fprintf(stderr,"%d\n",gdk_colormap_get_system_size());
     
     for(i=0;i<g->nwindow;i++){
-        /*Ginfoの内容をコピーする。*/
-        //w[i].colormap=gdk_colormap_new(gdk_visual_get_system(),TRUE);
         w[i].lastColor=g->numColors-1;
         for(j=0;j<g->numColors;j++){
             w[i].colortable[j].red=g->mastercolortable[j].red;
             w[i].colortable[j].green=g->mastercolortable[j].green;
             w[i].colortable[j].blue=g->mastercolortable[j].blue;
-            //gdk_colormap_alloc_color(w[i].colormap,&w[i].colortable[j],FALSE,FALSE);
         }
         w[i].window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	if(debug)fprintf(stderr,"Setup window X %d.\n",i);
-        gtk_widget_show(w[i].window);
 	if(debug)fprintf(stderr,"Setup window Y %d.\n",i);
         w[i].drawarea=gtk_drawing_area_new();
         gtk_container_add(GTK_CONTAINER(w[i].window),w[i].drawarea);
         gtk_drawing_area_size(GTK_DRAWING_AREA(w[i].drawarea),w[i].screenwidth,w[i].screenheight);
-        /*fprintf(stderr,"%d %d\n",w[i].screenwidth,w[i].screenheight);*/
 	if(debug)fprintf(stderr,"Setup window %d %d.\n",i,(int)w[i].drawarea);
         gtk_widget_show(w[i].drawarea);
-        gtk_signal_connect(GTK_OBJECT(w[i].drawarea),"expose_event",(GtkSignalFunc)expose_event,NULL);
-        gtk_signal_connect(GTK_OBJECT(w[i].drawarea),"configure_event",(GtkSignalFunc)configure_event,NULL);
-        gtk_signal_connect(GTK_OBJECT(w[i].window),"motion_notify_event",(GtkSignalFunc)motion_notify_event,NULL);
-        gtk_signal_connect(GTK_OBJECT(w[i].window),"button_press_event",(GtkSignalFunc)button_press_event,NULL);
-        gtk_signal_connect(GTK_OBJECT(w[i].window),"button_release_event",(GtkSignalFunc)button_release_event,NULL);
-        gtk_signal_connect(GTK_OBJECT(w[i].window),"key_press_event",(GtkSignalFunc)key_press_cb,NULL);
-	if(debug)fprintf(stderr,"Setup window B %d.\n",i);
+        gtk_signal_connect(GTK_OBJECT(w[i].drawarea),"expose_event",        (GtkSignalFunc)expose_event,NULL);
+        gtk_signal_connect(GTK_OBJECT(w[i].drawarea),"configure_event",     (GtkSignalFunc)configure_event,NULL);
+	if(debug)fprintf(stderr,"Setup window A0 %d.\n",i);
+        gtk_signal_connect(GTK_OBJECT(w[i].window),  "motion_notify_event", (GtkSignalFunc)motion_notify_event,NULL);
+	if(debug)fprintf(stderr,"Setup window A1 %d.\n",i);
+        gtk_signal_connect(GTK_OBJECT(w[i].window),  "button_press_event",  (GtkSignalFunc)button_press_event,NULL);
+	if(debug)fprintf(stderr,"Setup window A2 %d.\n",i);
+        gtk_signal_connect(GTK_OBJECT(w[i].window),  "button_release_event",(GtkSignalFunc)button_release_event,NULL);
+	if(debug)fprintf(stderr,"Setup window A3 %d.\n",i);
+        gtk_signal_connect(GTK_OBJECT(w[i].window),  "key_press_event",     (GtkSignalFunc)key_press_cb,NULL);
+	if(debug)fprintf(stderr,"Setup window A4 %d.\n",i);
         gtk_widget_set_events(w[i].window,
-                              GDK_STRUCTURE_MASK
+                                GDK_STRUCTURE_MASK
                               | GDK_EXPOSURE_MASK
                               | GDK_BUTTON_PRESS_MASK
                               | GDK_BUTTON_RELEASE_MASK
                               | GDK_KEY_PRESS_MASK
-                              | GDK_BUTTON1_MOTION_MASK);
+			      | GDK_POINTER_MOTION_MASK
+                              | GDK_BUTTON1_MOTION_MASK);// this causes an error.
 	if(debug)fprintf(stderr,"Setup window C %d.\n",i);
-        //gtk_widget_show(w[i].window);
+        gtk_widget_show(w[i].window);
 	if(debug)fprintf(stderr,"Setup window D %d.\n",i);
         gtk_window_set_title((GtkWindow *)w[i].window,w[i].inputfilename);
     }
     for(i=0;i<g->nwindow;i++){
 	if(debug)fprintf(stderr,"Setup window E %d.\n",i);
-        //w[i].pixmap=gdk_pixmap_new(w[i].window->window,w[i].screenwidth,w[i].screenheight,-1);
 	w[i].surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, w[i].screenwidth,w[i].screenheight);
 	if(debug)fprintf(stderr,"Setup window F %d %d.\n",i,(int)w[i].surface);
-        //cairo_t* cr  = gdk_cairo_create(w[i].pixmap);
         cairo_t* cr  = cairo_create(w[i].surface);
 	if(debug)fprintf(stderr,"Setup window G %d %d.\n",i, (int)cr);
         w[i].cr = cr;
@@ -673,7 +627,7 @@ void W_Init2(Winfo *w,Ginfo *g)
         cairo_select_font_face (cr, "Helvetica",
                                 CAIRO_FONT_SLANT_ITALIC ,
                                 CAIRO_FONT_WEIGHT_NORMAL);
-	cairo_set_antialias(cr, CAIRO_ANTIALIAS_NONE);//for speed
+	cairo_set_antialias(cr, CAIRO_ANTIALIAS_FAST);//for speed
 	if(debug)fprintf(stderr,"Setup window L %d.\n",i);
     }
     if(debug)fprintf(stderr,"Done settingup windows.\n");
