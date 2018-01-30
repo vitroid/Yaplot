01d13d34c5493b2c77f13f253e03061fb8a5d433
Masakazu Matsumoto <vitroid@gmail.com>
2018-01-30 17:48:37 +0900

    .

--- utils/yapmerge.pl
+++ utils/yapmerge.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
-#$B$=$N>l@_Dj(Bpalette$B$O!"=P$F$-$?=g$K?7HV9f$rM?$($l$P$h$$!#(B32$BHV0J9_$r;HMQ$9$k$3$H$K$9$k!#(B
-#$B%Q%l%C%HHV9f$N$=$N>l;XDj$O(Bglobal$B$G$"$k!#$D$^$j!":G=i$K0lEY@_Dj$9$l$P!"JQ99$5$l$J$$8B$j;}B3$9$k!#$3$N$?$a!"3F%U%!%$%k$K$*$1$k%Q%l%C%H0\$7BX$(I=$rJL8D$K=`Hw$7$J$1$l$P$J$i$J$$!#(B
+#その場設定paletteは、出てきた順に新番号を与えればよい。32番以降を使用することにする。
+#パレット番号のその場指定はglobalである。つまり、最初に一度設定すれば、変更されない限り持続する。このため、各ファイルにおけるパレット移し替え表を別個に準備しなければならない。
 use strict;
 use FileHandle;
 
@@ -16,11 +16,11 @@ my @offsets;
 while(1){
     $files[$nf]=new FileHandle;
     open $files[$nf],$ARGV[0];
-    shift;
+    shift(@ARGV);
     #open($files[$nf],shift)||usage;
     $nf ++;
     last if $#ARGV<0;
-    $offsets[$nf]=shift()+$offsets[$nf-1];
+    $offsets[$nf]=shift(@ARGV)+$offsets[$nf-1];
 }
 
 my $palette=32;
@@ -47,7 +47,7 @@ sub getput{
     while(<$files>){
 	return 0 if( /^\s*$/ );
 	if ( $init ){
-	      #$B$b$7%f!<%6$,FH<+?'$r3d$j$"$F$?$i!"JLHV9f$K3d$j$U$k!#(B
+	      #もしユーザが独自色を割りあてたら、別番号に割りふる。
 	  print "y ", $offsets+1, "\n";
 	  $init = 0;
 	}
